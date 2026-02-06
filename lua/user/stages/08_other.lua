42cd63b63159c74e8ec1511af72423f4e1412acd
visrust <madharchodambani@gmail.com>
2026-02-06 08:01:46 +0530

    fixed error due to path change

--- lua/user/stages/08_other.lua
+++ lua/user/stages/08_other.lua
@@ -1,12 +1,12 @@
 vim.defer_fn(function()
-    require('user.autopairs.autopairs')
+    require('user.other.autopairs.autopairs')
     -- require('autopairs.autopair_rule') -- already required in autopairs
 end, 300)
 
 vim.defer_fn(function()
-    require('user.keymaps.general')
+    require('user.other.keymaps.general')
 end, 300)
 
 vim.defer_fn(function()
-    require('autocmds.diagnostic')
+    require('user.other.autocmds.diagnostic')
 end, 300)
